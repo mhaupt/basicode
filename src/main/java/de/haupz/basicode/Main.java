748dd71a939f24448015d17f722020f944637f0c
Michael Haupt <github@haupz.de>
2023-11-04 16:05:08 +0100

    wire the demo Main to a Swing UI

--- src/main/java/de/haupz/basicode/Main.java
+++ src/main/java/de/haupz/basicode/Main.java
@@ -2,13 +2,18 @@ package de.haupz.basicode;
 
 import de.haupz.basicode.ast.ProgramNode;
 import de.haupz.basicode.interpreter.InterpreterState;
+import de.haupz.basicode.io.BufferedReaderInput;
 import de.haupz.basicode.parser.BasicParser;
+import de.haupz.basicode.ui.BasicContainer;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 
 public class Main {
+
+    static BasicContainer bc;
 
     public static void run(String code) throws Throwable {
         System.out.println("================================================================================");
@@ -16,14 +21,25 @@ public class Main {
         System.out.println("--------------------------------------------------------------------------------");
         final var parser = new BasicParser(new StringReader(code));
         ProgramNode prog = parser.program();
-        InterpreterState state =
-                new InterpreterState(prog, new BufferedReader(new InputStreamReader(System.in)), System.out);
+        InterpreterState state = new InterpreterState(prog,
+                new BufferedReaderInput(new BufferedReader(new InputStreamReader(System.in))),
+                bc);
         prog.run(state);
         System.out.println("================================================================================");
     }
 
     public static void main(String[] args) throws Throwable {
+        bc = new BasicContainer();
+        SwingUtilities.invokeLater(() -> gui());
         run("10 PRINT \"Hello, world!\"");
+    }
+
+    public static void gui() {
+        JFrame frame = new JFrame("BASICODE");
+        frame.setSize(bc.getSize());
+        frame.getContentPane().add(bc);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
 }
---
+++ src/main/java/de/haupz/basicode/ui/BasicContainer.java
@@ -0,0 +1,87 @@
+package de.haupz.basicode.ui;
+
+import de.haupz.basicode.io.BasicOutput;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+
+public class BasicContainer extends JComponent implements BasicOutput {
+
+    public static final int C_WIDTH = 14;
+
+    public static final int C_HEIGHT = 29;
+
+    public static final int COLUMNS = 40;
+
+    public static final int LINES = 25;
+
+    public static final int WIDTH = C_WIDTH * COLUMNS;
+
+    public static final int HEIGHT = C_HEIGHT * LINES;
+
+    public static final Font FONT = new Font("Monospaced", Font.BOLD, 24);
+
+    private final char[][] textBuffer = new char[LINES][COLUMNS];
+
+    private int curLine;
+
+    private int curColumn;
+
+    public BasicContainer() {
+        super();
+        setSize(WIDTH, HEIGHT);
+        clearTextBuffer();
+    }
+
+    public void clearTextBuffer() {
+        for (char[] line : textBuffer) {
+            Arrays.fill(line, ' ');
+        }
+        curLine = 0;
+        curColumn = 0;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        super.paintComponent(g2);
+        g2.setBackground(Color.BLUE);
+        g2.clearRect(0, 0, WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void paintChildren(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        super.paintChildren(g2);
+        g2.setFont(FONT);
+        g2.setColor(Color.YELLOW);
+        for (int l = 0; l < LINES; ++l) {
+            g2.drawChars(textBuffer[l], 0, COLUMNS, 0, (l+1) * C_HEIGHT);
+        }
+    }
+
+    @Override
+    public void print(String s) {
+        System.arraycopy(s.toCharArray(), 0, textBuffer[curLine], curColumn, s.length());
+        curColumn += s.length();
+    }
+
+    @Override
+    public void println(String s) {
+        print(s);
+        println();
+    }
+
+    @Override
+    public void println() {
+        curLine++;
+        curColumn = 0;
+    }
+
+    @Override
+    public void flush() {
+        // naught
+    }
+
+}
