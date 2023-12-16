776b3a3ae33e9cbccb669ad0af33988bba9ee2ba
Michael Haupt <github@haupz.de>
2023-12-16 10:10:57 +0100

    start documenting AST nodes (mostly: interpreter)

--- src/main/java/de/haupz/basicode/ast/BasicNode.java
+++ src/main/java/de/haupz/basicode/ast/BasicNode.java
@@ -2,10 +2,28 @@ package de.haupz.basicode.ast;
 
 import de.haupz.basicode.interpreter.InterpreterState;
 
+/**
+ * <p>The root class of all AST nodes used by the BASICODE interpreter.</p>
+ *
+ * <p>Each AST node either represents a statement (yielding no value) or an expression (yielding a value). Depending on
+ * the nature of a node, subclasses will override the {@link #run(InterpreterState)} or {@link #eval(InterpreterState)}
+ * method.</p>
+ */
 public abstract class BasicNode {
 
+    /**
+     * Execute a statement.
+     *
+     * @param state the interpreter state.
+     */
     public abstract void run(InterpreterState state);
 
+    /**
+     * Evaluate an expression and return its result.
+     *
+     * @param state the interpreter state.
+     * @return the value of the expression, given the interpreter state.
+     */
     public abstract Object eval(InterpreterState state);
 
 }
--- src/main/java/de/haupz/basicode/ast/LineNode.java
+++ src/main/java/de/haupz/basicode/ast/LineNode.java
@@ -4,31 +4,65 @@ import de.haupz.basicode.interpreter.InterpreterState;
 
 import java.util.List;
 
+/**
+ * The representation of a single line of code from a BASICODE program.
+ */
 public class LineNode extends BasicNode {
 
+    /**
+     * The source line number.
+     */
     private final int lineNumber;
 
+    /**
+     * All statements found on this line in the source code.
+     */
     private final List<StatementNode> statements;
 
+    /**
+     * Construct a {@code LineNode} from a line number and list of statements.
+     *
+     * @param lineNumber the line number.
+     * @param statements the statements.
+     */
     public LineNode(int lineNumber, List<StatementNode> statements) {
         this.lineNumber = lineNumber;
         this.statements = List.copyOf(statements);
     }
 
+    /**
+     * While a {@code LineNode} isn't an expression, it is also not seen as a statement. Execution of lines is done
+     * statement by statement in the {@link ProgramNode#run(InterpreterState)} interpreter loop. Consequently, this
+     * method just throws an exception.
+     *
+     * @param state the interpreter state.
+     */
     @Override
     public void run(InterpreterState state) {
         throw new IllegalStateException("a LineNode should not be run");
     }
 
+    /**
+     * Throw an exception, as a {@code LineNode} isn't an expression.
+     *
+     * @param state the interpreter state.
+     * @return nothing; throws an exception.
+     */
     @Override
     public Object eval(InterpreterState state) {
         throw new IllegalStateException("a LineNode should not be evaluated");
     }
 
+    /**
+     * @return this line's line number.
+     */
     public int getLineNumber() {
         return lineNumber;
     }
 
+    /**
+     * @return this line's list of statements.
+     */
     public List<StatementNode> getStatements() {
         return statements;
     }
