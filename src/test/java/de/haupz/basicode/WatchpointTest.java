b28fc48370bf9285252c2329bdc0194f94038a5b
Michael Haupt <github@haupz.de>
2025-11-13 17:49:43 +0100

    add test for watchpoint triggered multiple times

--- src/test/java/de/haupz/basicode/WatchpointTest.java
+++ src/test/java/de/haupz/basicode/WatchpointTest.java
@@ -22,4 +22,28 @@ public class WatchpointTest extends InterpreterTest {
                 """);
     }
 
+    @Test
+    public void testMultipleTriggers() {
+        testInterpreter("""
+                1000 GOTO 20
+                1010 DIM OD$(1):OD$(1)="A"
+                1020 OC$="A=2 OR A=4":GOSUB 965:PRINT "watchpoint";OP
+                1030 A=1:A=2:A=3:A=4:A=5
+                1040 GOTO 950
+                """, """
+                watchpoint 1\s
+                at line 1030, statement 1
+                1030 A=1:A=2:A=3:A=4:A=5
+                ---------^
+                == variables ==
+                A = 2.0
+                
+                at line 1030, statement 3
+                1030 A=1:A=2:A=3:A=4:A=5
+                -----------------^
+                == variables ==
+                A = 4.0\n
+                """);
+    }
+
 }
